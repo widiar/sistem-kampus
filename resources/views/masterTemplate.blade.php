601bcb9947e9b804e3630c53bdf279e6530e4166
YogiWiguna <58981369+YogiWiguna@users.noreply.github.com>
2021-10-02 14:53:15 +0800

    .

--- resources/views/masterTemplate.blade.php
+++ resources/views/masterTemplate.blade.php
@@ -82,7 +82,7 @@
                     <li><a href="{{ route('home') }}">Home</a></li>
                     <li><a href="{{ route('about') }}">About</a></li>
                     @guest
-                    <li><a href="{{ route('services') }}">MTA</a></li>
+                    
                     <li><a href="{{ route('login') }}">Login</a></li>
                     <li><a href="{{ route('register') }}">Register</a></li>
                     @endguest
