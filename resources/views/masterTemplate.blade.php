1683b21cafce4e08a0892338a7550e6a47977df8
widiar <awidiarsana.aw@gmail.com>
2021-10-12 11:01:47 +0800

    fixing footer

--- resources/views/masterTemplate.blade.php
+++ resources/views/masterTemplate.blade.php
@@ -111,14 +111,10 @@
 
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span>Eterna</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>AW</span></strong>. All Rights Reserved
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Created by <a href="http://ariwidiarsana-portofolio.herokuapp.com/">Ari Widiarsana</a>
             </div>
         </div>
     </footer><!-- End Footer -->
