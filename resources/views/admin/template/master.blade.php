6a1858923a77ad3d8de1382ca8a8370c00a518b9
widiar <awidiarsana.aw@gmail.com>
2021-12-21 22:18:18 +0800

    rev

--- resources/views/admin/template/master.blade.php
+++ resources/views/admin/template/master.blade.php
@@ -173,7 +173,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; <a href="http://ariwidiarsana-portofolio.herokuapp.com/">ITB STIKOM</a>
+                        <span>Copyright &copy; <a href="#">ITB STIKOM BALI KAMPUS JIMBARAN</a>
                             2021</span>
                     </div>
                 </div>
